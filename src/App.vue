fa28e5497b24aa87a497b46057daca6707d25011
Cherish Vuppala <cherishvuppala@gmail.com>
2025-08-21 15:12:13 +0530

    completed markdown editor

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from 'vue';
+import { marked } from 'marked';
+import { debounce } from 'lodash-es';
+
+const inputValue = ref('# hello')
+
+const output = computed(() => {
+  return marked(inputValue.value)
+})
+
+const update = debounce ((event) => {
+  inputValue.value = event.target.value;
+}, 100)
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="editor">
+    <textarea class="input" @input="update" :value="inputValue">
+    </textarea>
+
+    <div class="output" v-html="output"></div>
+
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+   body {
+    margin: 0;
+   }
+
+   .editor {
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+   }
+
+   .input, .output {
+    overflow: auto;
+    width: 50%;
+    height: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: none;
+    box-sizing: border-box;
+   }
+
+   .input {
+    background-color: #f0f0f0;
+    color: #333;
+   }
+
+    .output {
+      background-color: #fff;
+      color: #000;
+      overflow-y: auto;
+    }
+</style>
